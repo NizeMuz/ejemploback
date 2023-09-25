e220fcd5828b87241dd99ffbf6b219f7334638c9
NizeMuz <sebarod25@gmail.com>
2023-09-25 02:51:53 -0300

    app 2 terminada

---
+++ app2/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def vistaDos(request):
+    html="""<h1 style="color:blue">Hola Mundo vista2</h1>"""
+    
+    return HttpResponse(html)
